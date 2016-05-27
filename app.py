4f456e37421dfc97d2f2a4ce45eb88e53383f3a6
synsypa <changjiang23@gmail.com>
2016-05-27 03:47:50 -0400

    Added error handling for no features

--- app.py
+++ app.py
@@ -20,29 +20,32 @@ def index():
   else:
     ticker = request.form['ticker'].upper()
     features = request.form.getlist('features')
-
+   
     api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json?api_key=5wY62Y1qbsMcJA9qVjBz' % ticker
     session = requests.Session()
     session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
     raw_data = session.get(api_url)
 
     if len(raw_data.json()) <= 1:
-	return render_template('error.html', ticker=ticker)
+      return render_template('error.html', ticker=ticker)
 
     else:
-      cols = raw_data.json()[u'column_names']
-      data = raw_data.json()[u'data']
-      df = pd.DataFrame(data)
-      df.columns = cols
-      df['Date'] = pd.to_datetime(df['Date'])
+      if not features:
+        return render_template('blank.html')
+      else:
+        cols = raw_data.json()[u'column_names']
+        data = raw_data.json()[u'data']
+        df = pd.DataFrame(data)
+        df.columns = cols
+        df['Date'] = pd.to_datetime(df['Date'])
 
-      p = figure(title='Data from Quandl WIKI set',
- 		x_axis_label='date', x_axis_type='datetime')
-      for (f, c) in zip(features, Spectral4):
-        p.line(df['Date'], df[f], line_color=c, legend=ticker + ": " + f)    
+        p = figure(title='Data from Quandl WIKI set',
+		   x_axis_label='date', x_axis_type='datetime')
+        for (f, c) in zip(features, Spectral4):
+          p.line(df['Date'], df[f], line_color=c, legend=ticker + ": " + f)    
 
-      script, div = components(p)
-      return render_template('graph.html', script=script, div=div, ticker=ticker)
+        script, div = components(p)
+        return render_template('graph.html', script=script, div=div, ticker=ticker)
 
 if __name__ == '__main__':
 #  app.run(port=33507)
