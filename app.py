d3d454b7b852c0f8874163f40ebeee08522dc37b
synsypa <changjiang23@gmail.com>
2016-05-27 00:32:59 -0400

    edited app.py

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
 from flask import Flask, render_template, request, redirect
-from bokeh.plotting import figure
+from bokeh.plotting import figure, show
 from bokeh.embed import components
 import requests
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 app = Flask(__name__)
-app.vars = {}
+features = []
+ticker = ''
 
 @app.route('/')
 def main():
   return redirect('/index')
 
-@app.route('/index', methods=['POST'])
+@app.route('/index', methods=['GET','POST'])
 def index():
-  app.vars['ticker'] = request.form['ticker']
-  app.vars['features'] = request.form['features']
+  if request.method == 'GET':
+    return render_template('index.html')
+  else:
+    ticker = request.form['ticker']
+    features = request.form['features']
 
-  api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % app.vars['ticker']
+    return render_template('graph.html')
+
+@app.route('/graph', methods=['POST'])
+def graph():
+  api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % ticker
   session = requests.Session()
   session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
   raw_data = session.get(api.url)
 
-  plot = figure(tools=TOOLS, title='Data from Quandl WIKI set',
+  cols = raw_data.json()[u'column_names']
+  data = raw_data.json()[u'data']
+  df = pd.DataFrame(data)
+  df.columns = cols
+  df['Date'] = pd.to_datetime(df['Date'])
+
+  p = figure(tools=TOOLS, title='Data from Quandl WIKI set',
  		x_axis_label='date', x_axis_type='datetime')
 
   script, div = components(plot)
